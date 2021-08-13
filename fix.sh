a599d2904b9bd3da8a9f030bc93531416aaac2dd
Sunil Badyankal <sunilsbcloud@gmail.com>
2021-08-13 12:47:53 +1000

    deploy docs

--- fix.sh
+++ fix.sh
@@ -7,9 +7,9 @@ set -e
 mkdir practera-docs
 git clone --branch gh-pages https://$GITHUB_TOKEN@github.com/intersective/docs.git practera-docs/.
 
-# git config credential.helper 'cache --timeout=90'
-# git config --global user.name 'githubaction'
-# git config --global user.email 'githubaction@practera.com'
+git config credential.helper 'cache --timeout=90'
+git config --global user.name 'githubaction'
+git config --global user.email 'githubaction@practera.com'
 
 
 cp -R src/.vuepress/dist/* practera-docs/
